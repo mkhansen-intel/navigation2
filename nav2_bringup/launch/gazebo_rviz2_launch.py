43f04f8a763c85a42b013972956c7929669e9eb7
Matthew Hansen <matthew.k.hansen@intel.com>
2018-10-31 11:44:49 -0700

    WIP - doesn't work - Add arg for model_path

--- nav2_bringup/launch/gazebo_rviz2_launch.py
+++ nav2_bringup/launch/gazebo_rviz2_launch.py
@@ -1,13 +1,17 @@
 import os
 import sys
 from launch import LaunchDescription
+from launch.substitutions import EnvironmentVariable
 import launch.actions
 import launch_ros.actions
 
 def generate_launch_description():
     world = launch.substitutions.LaunchConfiguration('world')
+    model_path = launch.substitutions.LaunchConfiguration('model_path',
+								default=EnvironmentVariable(name='GAZEBO_MODEL_PATH'))
     return LaunchDescription([
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', world],
-        					output='screen'),
+					env={'GAZEBO_MODEL_PATH':model_path},
+        				output='screen'),
         launch_ros.actions.Node(package='rviz2', node_executable='rviz2')
 	])
